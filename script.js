e4ee4f843812961c6e98a61aab28e34c447262c5
mary-wan <mary.njenga@student.moringaschool.com>
2021-09-04 16:26:04 +0300

    add output message

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ function AkanGenerate(e, day, month, year) {
 
     if ((day < 1) || (day > 31) || isNaN(day)) {
         $(document.getElementById("invalid")).show();
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Day is invalid. Please enter a valid Day";
     } else if ((month < 1) || (month > 12) || isNaN(month)) {
         $(document.getElementById("invalid")).show();
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Month is invalid. Please enter a valid Month";
     } else if ((year < 1200) || isNaN(year)) {
         $(document.getElementById("invalid")).show();
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Year is invalid. Please enter a valid Year";
     } else if ((document.getElementById("male").checked == false) && (document.getElementById("female").checked == false)) {
         $(document.getElementById("invalid")).show();
+        $(document.getElementById("output")).hide();
         document.getElementById("invalid").innerHTML = "Please select your Gender";
     } else {
         $(document.getElementById("invalid")).hide();
@@ -27,10 +31,12 @@ function AkanGenerate(e, day, month, year) {
 
         if (document.getElementById("male").checked == true) {
             var nm = ml[date.getDay()]
-            alert("You were born on " + dayOfWeek + ".Your name is " + nm + ".")
+            $(document.getElementById("output")).show();
+            document.getElementById("output").innerHTML =("You were born on " + dayOfWeek + ".Your name is " + nm + ".")
         } else if (document.getElementById("female").checked == true) {
             var nm = fml[date.getDay()]
-            alert("You were born on " + dayOfWeek + ".Your name is " + nm + ".")
+            $(document.getElementById("output")).show();
+            document.getElementById("output").innerHTML =("You were born on " + dayOfWeek + ".Your name is " + nm + ".")
 
         }
 
@@ -39,4 +45,5 @@ function AkanGenerate(e, day, month, year) {
 /*Reset Button */
 function resetForm() {
     $(document.getElementById("invalid")).hide();
+    $(document.getElementById("output")).hide();
 }
